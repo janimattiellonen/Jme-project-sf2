1019023f0bc834812b16593ee4b7d34a58e7a47c
Janimatti Ellonen <janimatti.ellonen@janimatti.ellonen@gmail.com>
2012-02-11 01:01:13 +0200

    Added jsonSuccessResponse and jsonErrorResponse

--- library/Jme/Symfony/JmeSymfonyBundle/Controller/Controller.php
+++ library/Jme/Symfony/JmeSymfonyBundle/Controller/Controller.php
@@ -1,9 +1,48 @@
 <?php
 namespace Jme\Symfony\JmeSymfonyBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController,
+    Jme\Symfony\Component\JsonResponse;
 
 
 class Controller extends BaseController
 {
+    /**
+     *
+     * @param array $errors
+     * @param array $data 
+     * @param int $status http statuscode
+     * 
+     * @return Response
+     */
+    public function jsonErrorResponse(array $errors, array $data = array(), $status = 403 )
+    {
+        $arr = array(
+            'status' => 'FAIL',
+            'errors' => $errors,
+            'data'   => $data,
+        );
+        
+        $response = new JsonResponse(json_encode($arr), $status);
+        
+        return $response;
+    }
+    
+    /**
+     *
+     * @param array $data 
+     * 
+     * @return Response
+     */
+    public function jsonSuccessResponse(array $data = array() )
+    {
+        $arr = array(
+            'status' => 'SUCCESS',
+            'data'   => $data,
+        );
+        
+        $response = new JsonResponse(json_encode($arr), 200);
+        
+        return $response;
+    }
 }
